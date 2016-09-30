6fead6393b5dfa3d987cf9c41f0e13dc1e23ba84
Marco Capitani <m.capitani@nextworks.it>
2016-09-30 11:04:34 +0200

    Last round of bugfix (hopefully)

--- AppAffinity/src/main/java/it/nextworks/nephele/OFAAService/WebConfig.java
+++ AppAffinity/src/main/java/it/nextworks/nephele/OFAAService/WebConfig.java
@@ -2,17 +2,17 @@ package it.nextworks.nephele.OFAAService;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
-        registry.addMapping("/*/**");
+        registry.addMapping("/affinity/**");
+        registry.addMapping("/trafficmatrix/**");
+        registry.addMapping("/translate");
 
     }
 }
