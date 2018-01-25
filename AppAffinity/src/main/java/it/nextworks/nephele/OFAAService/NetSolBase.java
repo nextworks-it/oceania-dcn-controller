edb430086561b1028abec7c7b164903cfa094364
Marco Capitani <m.capitani@nextworks.it>
2018-01-25 15:36:17 +0100

    AppAff: catching stray exception in deserializing NetSolBase

--- AppAffinity/src/main/java/it/nextworks/nephele/OFAAService/NetSolBase.java
+++ AppAffinity/src/main/java/it/nextworks/nephele/OFAAService/NetSolBase.java
@@ -1,6 +1,7 @@
 package it.nextworks.nephele.OFAAService;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationContext;
@@ -41,41 +42,45 @@ public abstract class NetSolBase {
         @Override
         public NetSolBase deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
                 throws IOException {
-            ObjectCodec mapper = jsonParser.getCodec();
-            JsonNode node = mapper.readTree(jsonParser);
-            String allocId = node.get("Network_Allocation_ID").asText();
-            CompStatus status = CompStatus.valueOf(node.get("Status").asText());
-            String method;
-            if (!node.has("method")) {
-                method = "FULL";
-            } else {
-                method = node.get("method").asText();
-            }
-            switch (method) {
-                case "FULL":
-                    NetSolOutput solution = new NetSolOutput();
-                    solution.method = method;
-                    solution.status = status;
-                    solution.netAllocId = allocId;
-                    if ((status == CompStatus.COMPUTING) || (status == CompStatus.FAILED)) {
-                        solution.matrix = null;
-                    } else {
-                        solution.matrix = mapper.treeToValue(node.get("Network_Allocation_Solution"), int[][].class);
-                    }
-                    return solution;
-                case "INCREMENTAL":
-                    NetSolChanges changes = new NetSolChanges();
-                    changes.method = method;
-                    changes.status = status;
-                    changes.netAllocId = allocId;
-                    if ((status == CompStatus.COMPUTING) || (status == CompStatus.FAILED)) {
-                        changes.changes = null;
-                    } else {
-                        changes.changes = mapper.treeToValue(node.get("Network_Allocation_Solution"), int[][].class);
-                    }
-                    return changes;
-                default:
-                    throw new IllegalArgumentException(String.format("Unexpected method %s", method));
+            try {
+                ObjectCodec mapper = jsonParser.getCodec();
+                JsonNode node = mapper.readTree(jsonParser);
+                String allocId = node.get("Network_Allocation_ID").asText();
+                CompStatus status = CompStatus.valueOf(node.get("Status").asText());
+                String method;
+                if (!node.has("method")) {
+                    method = "FULL";
+                } else {
+                    method = node.get("method").asText();
+                }
+                switch (method) {
+                    case "FULL":
+                        NetSolOutput solution = new NetSolOutput();
+                        solution.method = method;
+                        solution.status = status;
+                        solution.netAllocId = allocId;
+                        if ((status == CompStatus.COMPUTING) || (status == CompStatus.FAILED)) {
+                            solution.matrix = null;
+                        } else {
+                            solution.matrix = mapper.treeToValue(node.get("Network_Allocation_Solution"), int[][].class);
+                        }
+                        return solution;
+                    case "INCREMENTAL":
+                        NetSolChanges changes = new NetSolChanges();
+                        changes.method = method;
+                        changes.status = status;
+                        changes.netAllocId = allocId;
+                        if ((status == CompStatus.COMPUTING) || (status == CompStatus.FAILED)) {
+                            changes.changes = null;
+                        } else {
+                            changes.changes = mapper.treeToValue(node.get("Network_Allocation_Solution"), int[][].class);
+                        }
+                        return changes;
+                    default:
+                        throw new IllegalArgumentException(String.format("Unexpected method %s", method));
+                }
+            } catch (Exception e) {
+                throw new JsonParseException(jsonParser, e.getMessage(), e);
             }
         }
     }
