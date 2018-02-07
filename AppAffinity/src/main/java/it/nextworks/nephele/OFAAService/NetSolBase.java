2ac8ccd9592206c396805811a39818c8692b5b6b
Marco Capitani <m.capitani@nextworks.it>
2018-02-07 10:46:33 +0100

    AppAff: fix for missing "method" field in "COMPUTING" response

--- AppAffinity/src/main/java/it/nextworks/nephele/OFAAService/NetSolBase.java
+++ AppAffinity/src/main/java/it/nextworks/nephele/OFAAService/NetSolBase.java
@@ -50,42 +50,51 @@ public abstract class NetSolBase {
                 JsonNode node = mapper.readTree(jsonParser);
                 String allocId = node.get("Network_Allocation_ID").asText();
                 CompStatus status = mapper.treeToValue(node.get("Status"), CompStatus.class);
-                String method;
-                method = node.get("Method").asText();
-                switch (method) {
-                    case "FULL":
-                        NetSolOutput solution = new NetSolOutput();
-                        solution.method = method;
-                        solution.status = status;
-                        solution.netAllocId = allocId;
-                        if ((status == CompStatus.COMPUTING) || (status == CompStatus.FAILED)) {
-                            solution.matrix = null;
-                        } else {
-                            try {
-                                solution.matrix = mapper.treeToValue(node.get("Network_Allocation_Solution"), int[][].class);
-                            } catch (NullPointerException exc) {
-                                // Ugly hack, should fix
-                                solution.matrix = mapper.treeToValue(node.get("Network_Allocation_Changes"), int[][].class);
+                if (status.equals(CompStatus.COMPUTED)) {
+                    String method;
+                    method = node.get("Method").asText();
+                    switch (method) {
+                        case "FULL":
+                            NetSolOutput solution = new NetSolOutput();
+                            solution.method = method;
+                            solution.status = status;
+                            solution.netAllocId = allocId;
+                            if ((status == CompStatus.COMPUTING) || (status == CompStatus.FAILED)) {
+                                solution.matrix = null;
+                            } else {
+                                try {
+                                    solution.matrix = mapper.treeToValue(node.get("Network_Allocation_Solution"), int[][].class);
+                                } catch (NullPointerException exc) {
+                                    // Ugly hack, should fix
+                                    solution.matrix = mapper.treeToValue(node.get("Network_Allocation_Changes"), int[][].class);
+                                }
                             }
-                        }
-                        return solution;
-                    case "INCREMENTAL":
-                        NetSolChanges changes = new NetSolChanges();
-                        changes.method = method;
-                        changes.status = status;
-                        changes.netAllocId = allocId;
-                        if ((status == CompStatus.COMPUTING) || (status == CompStatus.FAILED)) {
-                            changes.changes = null;
-                        } else {
-                            changes.changes = mapper.treeToValue(node.get("Network_Allocation_Changes"), int[][].class);
-                        }
-                        return changes;
-                    default:
-                        throw new IllegalArgumentException(String.format("Unexpected method %s", method));
+                            return solution;
+                        case "INCREMENTAL":
+                            NetSolChanges changes = new NetSolChanges();
+                            changes.method = method;
+                            changes.status = status;
+                            changes.netAllocId = allocId;
+                            if ((status == CompStatus.COMPUTING) || (status == CompStatus.FAILED)) {
+                                changes.changes = null;
+                            } else {
+                                changes.changes = mapper.treeToValue(node.get("Network_Allocation_Changes"), int[][].class);
+                            }
+                            return changes;
+                        default:
+                            throw new IllegalArgumentException(String.format("Unexpected method %s", method));
+                    }
+                } else {
+                    NetSolOutput output = new NetSolOutput(); // at random, not important
+                    output.method = null;
+                    output.status = status;
+                    output.matrix = null;
+                    return output;
                 }
             } catch (Exception e) {
                 throw new JsonParseException(jsonParser, e.getMessage(), e);
             }
+
         }
     }
 }
